f6571de55a35977481ace403bb55bcc096fe5d95
yoyo <hsjejxys@gmail.com>
2024-03-05 19:55:01 +0900

    Basic認証を追加

--- main.go
+++ main.go
@@ -1,12 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
 )
 
+// Basic認証
+func checkAuth(r *http.Request) bool {
+	// 認証情報取得
+	clientID, clientSecret, ok := r.BasicAuth()
+	if ok == false {
+		return false
+	}
+	return clientID == "client_id" && clientSecret == "client_secret"
+}
+
 func main() {
+
+	http.HandleFunc("/basic_auth",
+		func(w http.ResponseWriter, r *http.Request) {
+			// 認証失敗時
+			if checkAuth(r) == false {
+				w.Header().Add("WWW-Authenticate", `Basic realm="SECRET AREA"`)
+				w.WriteHeader(http.StatusUnauthorized) // 401
+				http.Error(w, "Unauthorized", 401)
+				return
+			}
+			_, err := fmt.Fprintf(w, "Successful Basic Authentication   \n")
+			if err != nil {
+				log.Fatal(err)
+			}
+		},
+	)
+	log.Println(http.ListenAndServe(":1323", nil))
+
 	e := echo.New()
 	e.Static("/static", "web/static")
 	// 404ページを表示する関数を実行
@@ -19,6 +51,30 @@ func main() {
 	e.GET("/termsOfService", getTermsOfService)
 	e.GET("/inquiry", getInquiry)
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func client() {
+	// クライアント生成
+	client := &http.Client{Timeout: time.Duration(30) * time.Second}
+	// リクエスト定義
+	req, err := http.NewRequest("GET", "http://localhost:1323/basic_auth", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 認証情報をセット
+	req.SetBasicAuth("client_id", "client_secret")
+	// リクエスト実行
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(b))
 }
 
 func getHome(c echo.Context) error {
